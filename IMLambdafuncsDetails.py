339e6648243342d3288f252237cf1fe7a0651c9d
abhinav-sehajpal21 <78021022+abhinav-sehajpal21@users.noreply.github.com>
2023-05-11 14:04:58 +0530

    Update IMLambdafuncsDetails.py

--- IMLambdafuncsDetails.py
+++ IMLambdafuncsDetails.py
@@ -4,7 +4,7 @@ import datetime
 import csv
 
 def lambda_handler(event, context):
-    
+
     # Get all Lambda functions
     client = boto3.client('lambda')
     functions = client.list_functions()['Functions']
@@ -48,25 +48,38 @@ def lambda_handler(event, context):
         
         function_details.append(function_dict)
 
-    # Write the function details to a CSV file
+    # Write the function details to a CSV file with timestamp in the filename
     IST = datetime.timezone(datetime.timedelta(hours=5, minutes=30))
-    filename = f"/tmp/Lambda-Inventory-{datetime.datetime.now(IST).strftime('%d-%m-%Y-%H-%M-%S')}.csv"
-    with open(filename, 'w') as csv_file:
+    timestamp_filename = f"/tmp/Lambda-Inventory-{datetime.datetime.now(IST).strftime('%d-%m-%Y-%H-%M-%S')}.csv"
+    with open(timestamp_filename, 'w') as csv_file:
         fieldnames = [IDENTIFIER, SERVICE, FUNCTION_NAME, DESCRIPTION, REGION, RUNTIME, MEMORY, TIMEOUT, LAST_MODIFIED, CODE_SIZE, ENV_VARS, TAGS]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         for function in function_details:
             writer.writerow(function)
 
-    # Upload the CSV file to S3
+    # Upload the CSV file with timestamp to S3
     s3 = boto3.client('s3')
     bucket_name = "bucketinventorymanagement"
-    folder_name = 'InventoryDetails'  
+    folder_name = 'InventoryDetails'
     if folder_name:
         s3.put_object(Bucket=bucket_name, Key=(folder_name+'/'))
 
-    s3.upload_file(filename, bucket_name, folder_name + '/' + os.path.basename(filename))
+    s3.upload_file(timestamp_filename, bucket_name, folder_name + '/' + os.path.basename(timestamp_filename))
+
+    # Write the function details to a CSV file with fixed name
+    fixed_filename = "/tmp/Latest-LambdaInventory.csv"
+    with open(fixed_filename, 'w') as csv_file:
+        fieldnames = [IDENTIFIER, SERVICE, FUNCTION_NAME, DESCRIPTION, REGION, RUNTIME, MEMORY, TIMEOUT, LAST_MODIFIED, CODE_SIZE, ENV_VARS, TAGS]
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for function in function_details:
+            writer.writerow(function)
+
+    # Upload the CSV file with fixed name to S3
+    s3.upload_file(fixed_filename, bucket_name, folder_name + '/Latest-LambdaInventory.csv')
+
     return {
         'statusCode': 200,
-        'body': f'Lambda Inventory file "{filename}" successfully generated and uploaded to S3 bucket "{bucket_name}"'
+        'body': f'Lambda Inventory files "{timestamp_filename}" and "{fixed_filename}" successfully generated and uploaded to S3 bucket "{bucket_name}"'
     }
